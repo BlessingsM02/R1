55d0d729906dffb70dec7873742def3bbfc43068
Blessings Mwandira <blessingsmwandira4@gmail.com>
2024-06-09 01:00:12 +0300

    Can send their location to firebase

---
+++ Roadside Rescue 2/ViewModel/FirebaseService.cs
@@ -0,0 +1,32 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Threading.Tasks;
+
+namespace Roadside_Rescue_2.ViewModel
+{
+    public class FirebaseService
+    {
+        private readonly FirebaseClient _firebaseClient;
+
+        public FirebaseService()
+        {
+            // Initialize FirebaseClient with your Firebase database URL
+            _firebaseClient = new FirebaseClient("https://final-year-c48d0-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task SendCoordinatesAsync(double latitude, double longitude)
+        {
+            var coordinate = new
+            {
+                Latitude = latitude,
+                Longitude = longitude,
+                Timestamp = DateTime.UtcNow
+            };
+
+            await _firebaseClient
+                .Child("locations")
+                .PostAsync(coordinate);
+        }
+    }
+}
--- Roadside Rescue 2/Views/HomePage.xaml.cs
+++ Roadside Rescue 2/Views/HomePage.xaml.cs
@@ -1,9 +1,53 @@
+
+using Roadside_Rescue_2.ViewModel;
+
 namespace Roadside_Rescue_2.Views;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
+    private readonly FirebaseService _firebaseService;
+    public HomePage()
 	{
 		InitializeComponent();
-	}
+        _firebaseService = new FirebaseService();
+    }
+    private async void OnGetLocationClicked(object sender, EventArgs e)
+    {
+        await GetAndSendLocationAsync();
+    }
+
+    private async Task GetAndSendLocationAsync()
+    {
+        try
+        {
+            var location = await Geolocation.GetLastKnownLocationAsync();
+
+            if (location != null)
+            {
+                LocationLabel.Text = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}";
+
+                // Send coordinates to Firebase
+                await _firebaseService.SendCoordinatesAsync(location.Latitude, location.Longitude);
+            }
+            else
+            {
+                LocationLabel.Text = "No location data available";
+            }
+        }
+        catch (FeatureNotSupportedException fnsEx)
+        {
+            // Handle not supported on device exception
+            LocationLabel.Text = "Feature not supported on device";
+        }
+        catch (PermissionException pEx)
+        {
+            // Handle permission exception
+            LocationLabel.Text = "Permission to access location was denied";
+        }
+        catch (Exception ex)
+        {
+            // Unable to get location
+            LocationLabel.Text = $"Unable to get location: {ex.Message}";
+        }
+    }
 }
